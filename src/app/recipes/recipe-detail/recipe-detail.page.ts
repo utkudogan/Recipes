de174d80dd72fad23e4508eb71ef7b1840267e31
utkudogan <52001660+utkudogan@users.noreply.github.com>
2021-01-30 17:44:54 +0300

    alert ve yeni component eklendi
    alert eklendi. yeni component yapıldı ve uygulamada kullanıldı

--- src/app/recipes/recipe-detail/recipe-detail.page.ts
+++ src/app/recipes/recipe-detail/recipe-detail.page.ts
@@ -1,35 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Recipe } from '../recipe.model';
-import { RecipesService } from '../recipes.service';
-
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AlertController } from "@ionic/angular";
+import { Recipe } from "../recipe.model";
+import { RecipesService } from "../recipes.service";
 
 @Component({
-  selector: 'app-recipe-detail',
-  templateUrl: './recipe-detail.page.html',
-  styleUrls: ['./recipe-detail.page.scss'],
+  selector: "app-recipe-detail",
+  templateUrl: "./recipe-detail.page.html",
+  styleUrls: ["./recipe-detail.page.scss"],
 })
 export class RecipeDetailPage implements OnInit {
-loadedRecipe: Recipe;
+  loadedRecipe: Recipe;
   constructor(
-    private activatedRout:ActivatedRoute, 
-    private recipesService:RecipesService,
-    private router:Router
-    ) { }
+    private activatedRout: ActivatedRoute,
+    private recipesService: RecipesService,
+    private router: Router,
+    private alertController: AlertController
+  ) {}
 
   ngOnInit() {
-    this.activatedRout.paramMap.subscribe(paramMap => {
-      if (!paramMap.has('recipeId')) {
+    this.activatedRout.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has("recipeId")) {
+        this.router.navigate(["/recipes"]);
         return;
       }
-      const recipeId = paramMap.get('recipeId');
-      this.loadedRecipe = this.recipesService.getRecipe(recipeId)
-    })
+      const recipeId = paramMap.get("recipeId");
+      this.loadedRecipe = this.recipesService.getRecipe(recipeId);
+    });
   }
 
-  deleteRecipe(){
-    this.recipesService.deleteRecipe(this.loadedRecipe.id);
-    this.router.navigate(['/recipes']);
-  }
+  async deleteRecipe() {
+    const alert = await this.alertController.create({
+      cssClass: "my-custom-class",
+      header: "Confirm!",
+      message: "Message <strong>text</strong>!!!",
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          cssClass: "secondary",
+          
+        },
+        {
+          text: "Delete",
+          handler: () => {
+            this.recipesService.deleteRecipe(this.loadedRecipe.id);
+            this.router.navigate(["/recipes"]);
+          },
+        },
+      ],
+    });
 
+    await alert.present();
+  }
 }
